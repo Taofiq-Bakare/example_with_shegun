80c2b9a6e166af491696fea5703d27af4b20c88e
USERNAME <taofiq01@gmail.com>
2022-02-22 21:11:00 +0100

    I created a new file to show shegun how software dev works

---
+++ example.py
@@ -0,0 +1,12 @@
+# This is an example file
+
+# Python types
+
+integers = 27
+decimal = 60.1
+character = 'A'
+strings_word = "Shegun"
+boolean1 = True
+boolean2 = False
+
+print(f"My name is {strings_word} and I am {integers} years old")
